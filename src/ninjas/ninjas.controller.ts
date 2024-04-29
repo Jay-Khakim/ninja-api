9f27daa7e9448088345c59a03bb6c5cf54c43759
Jay Khakim <mgmediajay@gmail.com>
2024-04-29 09:53:34 +0900

    FEAT: getNinjas

---
+++ src/ninjas/ninjas.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Query } from '@nestjs/common';
+
+@Controller('ninjas')
+export class NinjasController {
+    @Get()
+    getNinjas(@Query('type') type: string){
+        return [{type}]
+    }
+
+
+    @Get("id")
+    getOneNinja(){
+        return {}
+    }
+}
+
+
